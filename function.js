8c9d4792e1ee0a06f8fdd9f731536715b0ce218d
mahimur <hellomahimur@gmail.com>
2022-08-04 15:05:54 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,10 @@
+//function declaration
+function startFan() {
+    console.log('Switch the fan');
+}
+//call the function
+startFan();
+console.log(12);
+startFan();
+console.log(12 < 5);
+startFan();
---
+++ parameter.js
@@ -0,0 +1,24 @@
+/* function bringSingara(money) {
+    console.log('mama singara denn');
+    console.log(money);
+}
+bringSingara(50); */
+
+function bringSingara(money) {
+    console.log('mama singara denn');
+    console.log('tk disen:', money);
+}
+var taka = 70;
+bringSingara(taka);
+// add
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+add(78, 34);
+// sum
+function sum(a, b, c, d, e) {
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+sum(12, 14, 16, 76, 98);
