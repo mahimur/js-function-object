f0e6265c96fbcd7e3f9d65fe81922dd08e5f047f
mahimur <hellomahimur@gmail.com>
2022-08-04 15:34:44 +0600

    second commit

---
+++ object.js
@@ -0,0 +1,25 @@
+var student = {
+    id: 192,
+    name: 'Solaiman Khan',
+    class: 9,
+    marks: 58
+}
+
+var mobile = {
+    brand: 'Samsung',
+    price: 15990,
+    camera: '32MP',
+    storage: '32GB'
+}
+
+var myLaptop = {
+    brand: 'Asus',
+    price: 39000,
+    color: 'silver',
+    processor: 'amd razen 3'
+}
+console.log(myLaptop.price);
+console.log(myLaptop.brand);
+console.log(myLaptop);
+myLaptop.processor = 'i7';
+console.log(myLaptop);
